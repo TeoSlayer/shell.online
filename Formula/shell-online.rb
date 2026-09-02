876d61ad87c264fb87924b7c0e42687516d6aa6c
Teodor Calin <teodor@vulturelabs.io>
2026-09-02 22:17:12 +0300

    Point Homebrew at v0.7.1

--- Formula/shell-online.rb
+++ Formula/shell-online.rb
@@ -1,8 +1,8 @@
 class ShellOnline < Formula
   desc "Turn any terminal process into an interactive or read-only browser link"
   homepage "https://shell.online"
-  url "https://github.com/TeoSlayer/shell.online/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "8eae46638843719c97bb214a8b6a9b5a9a8677386a54ce842f00e61af9c94da6"
+  url "https://github.com/TeoSlayer/shell.online/archive/refs/tags/v0.7.1.tar.gz"
+  sha256 "becdd475f135d6b2c09ea94131c95dabc819ab9b3d729fb3a56b9835db8d4412"
   license "MIT"
 
   depends_on "go" => :build
